e8a919f7d90fdd32f1f7b3bb7c6277ae9e3eeaca
leonitdev <leonitkuleta@gmail.com>
2023-01-23 15:11:05 +0100

    bugfix wrong import

--- src/components/buttons/ButtonSave.tsx
+++ src/components/buttons/ButtonSave.tsx
@@ -1,6 +1,5 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import {colors} from '../../styles';
 
 interface ButtonSaveProps {
   save: () => any;
@@ -22,7 +21,7 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   buttonSave: {
-    backgroundColor: colors.black,
+    backgroundColor: '#2A2A2A',
     paddingVertical: 12,
     textAlign: 'center',
     justifyContent: 'center',
@@ -31,7 +30,7 @@ const styles = StyleSheet.create({
   },
 
   buttonSaveText: {
-    color: colors.white,
+    color: '#FFFFFF',
     fontSize: 14,
     fontWeight: '500',
     lineHeight: 21,
